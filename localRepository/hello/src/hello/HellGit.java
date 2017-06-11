e1a0a23fc9e06cbeedd7ffb40b44b6d6cbf789a3
admin <chenmq@hjsoft.com.cn>
2017-06-11 14:34:38 +0800

    增加HellGit.java

---
+++ localRepository/hello/src/hello/HellGit.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package hello;
+
+/**
+ * <p>Title:HellGit.java</p>
+ * <p>Description>:</p>
+ * <p>Company:HJSJ</p>
+ * <p>Create Time:2017年6月11日 下午2:33:20</p>
+ * <p>@version: 1.0</p>
+ * <p>@author:chenmq
+ */
+public class HellGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("I am coming ,Git.");
+
+	}
+
+}
